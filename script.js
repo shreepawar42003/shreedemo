9bb79690e1efe5bbc712eb7db710564ef9cc8c4b
shreepawar42003 <shreepawar417@gmail.com>
2024-12-25 12:52:53 +0530

    new

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let btn = document.querySelector("#mode");
+let modebtn = document.querySelector("#mode");
 let currmode = "light";
 let body = document.querySelector("body");
 
-btn.addEventListener("click", () => {
-  if (currmode === light) {
-    currmode = "dark";
-    body.style.backgroundColor = " black";
-  } 
-  else {
+modebtn.addEventListener("click", () => {
+  if (currmode === "light") {
+    currmode = "drak";
+    body.style.backgroundColor = "black";
+  } else {
     currmode = "light";
-    body.style.backgroundColor = " white";
+    body.style.backgroundColor = "white";
   }
   console.log(currmode);
 });
